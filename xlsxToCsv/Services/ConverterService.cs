2347dfa7bfd47a0d07632e3d5f349f8be74a031c
alehee <aleksanderheese@o2.pl>
2023-07-02 23:22:12 +0200

    preparing parsing module

--- xlsxToCsv/Services/ConverterService.cs
+++ xlsxToCsv/Services/ConverterService.cs
@@ -8,12 +8,21 @@
             {
                 try
                 {
-
+                    var sheets = GetSheetsData(file);
                 } catch (Exception e)
                 {
-
+                    Console.WriteLine($" An error occured while parsing `{file}`. Ignoring this file.");
                 }
             }
+        }
+
+        private static Dictionary<string, List<List<object>>> GetSheetsData(string filePath)
+        {
+            var dict = new Dictionary<string, List<List<object>>>();
+
+
+
+            return dict;
         }
     }
 }
